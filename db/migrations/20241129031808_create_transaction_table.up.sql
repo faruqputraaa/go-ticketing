e897e341b5bd8b7f6edf641bac7bfbf751c48c02
faruqputraaa <fafaputra999@gmail.com>
2024-11-29 19:57:44 +0700

    membuat migration transactions

---
+++ db/migrations/20241129031808_create_transaction_table.up.sql
@@ -0,0 +1,16 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS transactions(
+    id_transaction INT PRIMARY KEY,
+    id_user INT NOT NULL,
+    quantity_ticket INT NOT NULL,
+    id_event INT NOT NULL,
+    id_ticket INT NOT NULL,
+    total_price FLOAT,
+    date_order TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (id_user) REFERENCES users(id_user),
+    FOREIGN KEY (id_event) REFERENCES events(id_events)
+    FOREIGN KEY (id_ticket) REFERENCES tickets(id_ticket)
+)
