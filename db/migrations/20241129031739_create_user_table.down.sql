8931e1ed1a01f2d539582e3d964633e0646b39b9
Angga Kusuma <kusumadiatmika02@gmail.com>
2024-11-29 21:06:58 +0700

    update user table

--- db/migrations/20241129031739_create_user_table.down.sql
+++ db/migrations/20241129031739_create_user_table.down.sql
@@ -1,2 +1,9 @@
 CREATE TABLE IF NOT EXISTS transactions(
-  id_transaction INT PRIMARY KEY,)
+  id_transaction INT PRIMARY KEY,
+  id_user INT NOT NULL,
+  quantity_ticket INT NOT NULL,
+  id_event INT NOT NULL,
+  id_ticket INT NOT NULL,
+  total_price FLOAT,
+  date_order TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,)
