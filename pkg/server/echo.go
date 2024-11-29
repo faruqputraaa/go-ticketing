cc1d0505d7e47d5ce180912d88764b0941e970d6
faruqputraaa <fafaputra999@gmail.com>
2024-11-29 10:32:00 +0700

    fix folder directory

---
+++ pkg/server/echo.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"github.com/faruqputraaa/go-ticket/pkg/route"
+	"github.com/labstack/echo/v4"
+)
+
+type Server struct {
+	*echo.Echo
+}
+
+func NewServer(publicRoutes, privateRoutes []route.Route) *Server {
+	e := echo.New()
+	v1 := e.Group("api/v1")
+	if len(publicRoutes) > 0 {
+		for _, route := range publicRoutes {
+			v1.Add(route.Method, route.Path, route.Handler)
+		}
+	}
+	if len(privateRoutes) > 0 {
+		for _, route := range privateRoutes {
+			v1.Add(route.Method, route.Path, route.Handler)
+		}
+	}
+	return &Server{e}
+}
