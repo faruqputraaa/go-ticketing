c6bdfd88737e2937934ec96d55dc032753c33adf
faruqputraaa <fafaputra999@gmail.com>
2024-12-03 14:07:39 +0700

    create table offers

---
+++ db/migrations/20241203065501_create_offer_table.up.sql
@@ -0,0 +1,12 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS offers(
+    id_offer INT PRIMARY KEY AUTO_INCREMENT,
+    id_user INT NOT NULL,
+    name_event VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
+
+    FOREIGN KEY (id_user) REFERENCES users(id_user)
+);
+
+COMMIT;
