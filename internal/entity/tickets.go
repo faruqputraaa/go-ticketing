bded5f1233018a25a0b0041472701663fc73a849
Eliza Deani <elizadeani03@gmail.com>
2024-12-07 02:16:29 +0700

    nambahin entity ticket

--- internal/entity/tickets.go
+++ internal/entity/tickets.go
@@ -1,6 +1,7 @@
 package entity
 
 type Ticket struct {
+	IDTicket int64  `json:"id_ticket" gorm:"primary_key;auto_increment"`
 	IDEvent  int64  `json:"id_event"`
 	Price    int64  `json:"price"`
 	Category string `json:"category"` //jenis tiket
