c4706964d1f33031f3eea65d266d27bd86dbabc5
faruqputraaa <fafaputra999@gmail.com>
2024-12-08 01:29:48 +0700

    membuat field email di users dan offers

--- db/migrations/20241129031808_create_transaction_table.down.sql
+++ db/migrations/20241129031808_create_transaction_table.down.sql
@@ -1,5 +1,5 @@
 BEGIN;
 
-DROP TABLE IF EXISTS transaction;
+DROP TABLE IF EXISTS transactions;
 
 COMMIT;
--- db/migrations/20241203065501_create_offer_table.up.sql
+++ db/migrations/20241203065501_create_offer_table.up.sql
@@ -3,6 +3,7 @@ BEGIN;
 CREATE TABLE IF NOT EXISTS offers(
     id_offer BIGSERIAL PRIMARY KEY,
     id_user INT NOT NULL,
+    email VARCHAR(255) NOT NULL,
     name_event VARCHAR(255) NOT NULL,
     description TEXT NOT NULL,
 
